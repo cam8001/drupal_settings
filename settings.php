8cc10d1f1d8f6c4c38a7bca92723dead0200f535
Cameron Tod <cameron.tod@gmail.com>
2015-11-26 22:20:44 +0000

    Added Fast 404

--- settings.php
+++ settings.php
@@ -37,6 +37,11 @@ if (file_exists($ac_settings_path)) {
 $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
+$config['system.performance']['fast_404']['exclude_paths'] = '/\/(?:styles)|(?:system\/files)\//';
+$config['system.performance']['fast_404']['paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+$config['system.performance']['fast_404']['html'] = '<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
+
+
 /**
  * Load local development override configuration, if available.
  *
