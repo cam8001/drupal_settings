8e71f205a73be7f4cead209c3065f941c48eac80
Cameron Tod <cameron.tod@gmail.com>
2018-06-01 12:54:39 +1200

    Removed deprecated dirname function

--- settings.php
+++ settings.php
@@ -83,7 +83,7 @@ drupal_fast_404();
 /**
  * Include a local settings file if it exists.
  */
-$local_settings = dirname(__FILE__) . '/settings.local.php';
+$local_settings = __DIR__ . '/settings.local.php';
 if (file_exists($local_settings)) {
   include $local_settings;
 }
